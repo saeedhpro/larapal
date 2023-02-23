1e3abfad8c039bb5ee1528fc21a7f623843b6d51
saeed heydari <saeedhpro@gmail.com>
2023-02-23 16:33:57 +0330

    Fix

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,6 +7,7 @@ use App\Models\OldProducts;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Intervention\Image\Facades\Image;
 use Maatwebsite\Excel\Facades\Excel;
 
 class Controller extends BaseController
@@ -15,17 +16,28 @@ class Controller extends BaseController
 
     public function index()
     {
-        $list = OldProducts::query()->get('main_picture')->toArray();
-        $list2 = OldProducts::query()->whereNotNull("pictures")->get('pictures')->toArray();
+        $old = OldProducts::query()->take(5);
+        $list = $old->pluck('main_picture');
+        $list2 = $old->whereNotNull("pictures")->get('pictures')->toArray();
         foreach ($list2 as $l) {
             foreach ($l as $l2) {
                 $a = json_decode($l2);
                 foreach ($a as $l3) {
-                    $list[] = ['' => $l3 ];
+                    $list[] = $l3;
                 }
             }
         }
-        $export = new OldProductExport($list);
-        return Excel::download($export, 'main_product_images.xlsx');
+        foreach ($list as $l) {
+            $p = public_path($l);
+            $path = "E:\\old/" . $l;
+            if (file_exists($l)) {
+                echo $l;
+                $image = Image::make($p);
+                $image->save($path);
+            }
+        }
+        return true;
+//        $export = new OldProductExport($list);
+//        return Excel::download($export, 'main_product_images.xlsx');
     }
 }
